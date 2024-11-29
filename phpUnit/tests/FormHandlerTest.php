f6acabfb0a584a5825c2fc90dc75107c903fa30a
leonidaspedro <leonidas@unochapeco.edu.br>
2024-11-29 16:41:54 -0300

    pra entregar

--- phpUnit/tests/FormHandlerTest.php
+++ phpUnit/tests/FormHandlerTest.php
@@ -1,27 +1,27 @@
- <?php
+<?php
 
-   use PHPUnit\Framework\TestCase;
-   use App\FormHandler;
+use PHPUnit\Framework\TestCase;
+use App\FormHandler;
 
-   class FormHandlerTest extends TestCase
-   {
-       public function testValidateName()
-       {
-           $formHandler = new FormHandler();
+class FormHandlerTest extends TestCase
+{
+    public function testValidateName()
+    {
+        $formHandler = new FormHandler();
 
-           $this->assertFalse($formHandler->validateName('John'));
-           $this->assertTrue($formHandler->validateName('John Doe'));
-       }
+        $this->assertFalse($formHandler->validateName('Zé'));
+        $this->assertTrue($formHandler->validateName('José Fernando da Silva'));
+    }
 
-       public function testSaveData()
-       {
-           $formHandler = new FormHandler();
+    public function testSaveData()
+    {
+        $formHandler = new FormHandler();
 
-           $result = $formHandler->saveData('John Doe', '2000-01-01');
-           
-           $this->assertArrayHasKey('name', $result);
-           $this->assertEquals('John Doe', $result['name']);
-           $this->assertInstanceOf(\DateTime::class, $result['date']);
-           $this->assertEquals('2000-01-01', $result['date']->format('Y-m-d'));
-       }
-   }
+        $result = $formHandler->saveData('José Fernando da Silva', '2000-01-01');
+        
+        $this->assertArrayHasKey('name', $result);
+        $this->assertEquals('José Fernando da Silva', $result['name']);
+        $this->assertInstanceOf(\DateTime::class, $result['date']);
+        $this->assertEquals('2000-01-01', $result['date']->format('Y-m-d'));
+    }
+} 
